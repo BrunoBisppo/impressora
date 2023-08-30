c7d42420db0fbcea887b6af956088b57f159629e
BrunoBisppo <Bisppobruno@gmail.com>
2023-08-30 20:46:07 -0300

    new function in js
    nova função de javascript

--- script.js
+++ script.js
@@ -1,13 +1,28 @@
 const searchInput = document.getElementById('searchInput');
-const printerList = document.getElementById('printerList').getElementsByClassName('printer');
+const printerList = document.getElementById('printerList');
+const printers = printerList.getElementsByClassName('printer');
+
+function sortPrintersAlphabetically() {
+    const printerArray = Array.from(printers);
+
+    printerArray.sort((a, b) => {
+        const nameA = a.querySelector('.printer-name').textContent.toLowerCase();
+        const nameB = b.querySelector('.printer-name').textContent.toLowerCase();
+        return nameA.localeCompare(nameB);
+    });
+
+    for (const printer of printerArray) {
+        printerList.appendChild(printer);
+    }
+}
 
 searchInput.addEventListener('input', function () {
     const searchTerm = searchInput.value.toLowerCase();
-    
-    for (const printer of printerList) {
+
+    for (const printer of printers) {
         const printerName = printer.querySelector('.printer-name').textContent.toLowerCase();
         const printerToner = printer.querySelector('.printer-toner').textContent.toLowerCase();
-        
+
         if (printerName.includes(searchTerm) || printerToner.includes(searchTerm)) {
             printer.style.display = 'block';
         } else {
@@ -15,3 +30,6 @@ searchInput.addEventListener('input', function () {
         }
     }
 });
+
+// Chame a função de ordenação inicialmente
+sortPrintersAlphabetically();
